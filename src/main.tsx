6db6b0ce42e308266f10c2e619adcda63a262cee
Yugo Kawai <yugo6581@g.ecc.u-tokyo.ac.jp>
2026-09-04 18:28:26 +0900

    Apply fixed-canvas sidebar layout override

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './styles.css'
+import './layout-fixes.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
