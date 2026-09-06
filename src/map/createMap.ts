0118b27669490e21bf0ac8f4ce3d900f5d35c1b2
Yugo Kawai <yugo6581@g.ecc.u-tokyo.ac.jp>
2026-09-06 17:46:18 +0900

    Try OpenFreeMap Bright for richer modern labels

--- src/map/createMap.ts
+++ src/map/createMap.ts
@@ -1,7 +1,7 @@
 import maplibregl from 'maplibre-gl'
 import { TOKYO_CAMERA } from './config'
 
-export const PRESENTATION_STYLE_URL = 'https://tiles.openfreemap.org/styles/positron'
+export const PRESENTATION_STYLE_URL = 'https://tiles.openfreemap.org/styles/bright'
 
 export function createMap(container: HTMLElement, pixelRatio: number): maplibregl.Map {
   return new maplibregl.Map({ container, style: PRESENTATION_STYLE_URL, ...TOKYO_CAMERA, pixelRatio, canvasContextAttributes: { preserveDrawingBuffer: true }, attributionControl: false, maxPitch: 60 })
