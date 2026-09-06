1d512ac8ea734316c1ce17fbe9983b3b8af32ebd
ykawai6581 <yugo6581@g.ecc.u-tokyo.ac.jp>
2026-09-06 19:35:25 +0900

    update

--- src/map/highlightDefaults.ts
+++ src/map/highlightDefaults.ts
@@ -1,7 +1,7 @@
 import type { HighlightStyle } from '../types/geo'
 
 export const ROAD_LABEL_COLOR = '#FF7B00'
-export const HISTORICAL_ROAD_LABEL_COLOR = '#5C3838'
+export const HISTORICAL_ROAD_LABEL_COLOR = '#831B1B'
 export const REGION_HIGHLIGHT_COLOR = '#C84646'
 export const JURISDICTION_HIGHLIGHT_COLOR = '#FFFFFF'
 export const ROAD_LABEL_HALO_COLOR = '#FFFFFF'
@@ -9,7 +9,7 @@ export const ROAD_LABEL_HALO_WIDTH = 3
 export const ACTIVE_LINE_CASING_EXTRA_WIDTH = 6
 export const ACTIVE_LINE_SHADOW_EXTRA_WIDTH = 14
 export const STATION_LABEL_COLOR = '#65668F'
-export const SHUKUBA_LABEL_COLOR = '#7F612A'
+export const SHUKUBA_LABEL_COLOR = '#831B1B'
 
 export const DEFAULT_HIGHLIGHT_STYLE: HighlightStyle = {
   roadColor: ROAD_LABEL_COLOR,
